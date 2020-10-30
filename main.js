a729ae940daa773a113ce521492319b1e6cde814
東遼太 <ryotahigashi318@gmail.com>
2020-10-30 20:53:38 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,498 @@
+'use strict'
+
+function defaultItems(index){
+  if (index===0) {
+    return[{
+        "item": "テント",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "ペグ",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "クーラーボックス",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "焚き火台",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "着火用ライター",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "BBQ用の網",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "ランタン",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "包丁とまな板",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "食器",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "折り畳みイス",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "トング",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "革手袋",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "スポンジ",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "寝袋",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "歯ブラシ",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "テントマット",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "ガスカートリッジ",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "炭、薪",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "保冷剤",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+        "item": "タオル",
+        "check": false,
+        "heart": "♥♥"
+      },
+      {
+        "item": "着替え",
+        "check": false,
+        "heart": "♥♥"
+      },
+      {
+        "item": "折り畳み傘",
+        "check": false,
+        "heart": "♥♥"
+      },
+      {
+        "item": "タープ",
+        "check": false,
+        "heart": "♥♥"
+      },
+      {
+        "item": "テーブル",
+        "check": false,
+        "heart": "♥♥"
+      },
+      {
+        "item": "帽子",
+        "check": false,
+        "heart": "♥"
+      },
+      {
+        "item": "虫除けスプレー",
+        "check": false,
+        "heart": "♥"
+      },
+      {
+        "item": "洗剤",
+        "check": false,
+        "heart": "♥"
+      },
+    ]
+  }else if (index===1) {
+    return[{
+      "item": "パスポート",
+      "check": false,
+      "heart": "♥♥♥"
+    },
+    {
+      "item": "財布",
+      "check": false,
+      "heart": "♥♥♥"
+    },
+    {
+      "item": "スマホ",
+      "check": false,
+      "heart": "♥♥♥"
+    },
+    {
+      "item": "常備薬",
+      "check": false,
+      "heart": "♥♥♥"
+    },
+    {
+      "item": "時計",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "カメラ",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "充電器",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "観光用バッグ",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "折り畳み傘",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "着替え",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "水着",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "タオル",
+      "check": false,
+      "heart": "♥"
+    },
+    {
+      "item": "サングラス",
+      "check": false,
+      "heart": "♥"
+    },
+    {
+      "item": "日焼け止め",
+      "check": false,
+      "heart": "♥"
+    },
+    {
+      "item": "サンダル",
+      "check": false,
+      "heart": "♥"
+    },
+    {
+      "item": "帽子",
+      "check": false,
+      "heart": "♥"
+    },
+    {
+      "item": "整髪料",
+      "check": false,
+      "heart": "♥"
+    },
+    {
+      "item": "歯ブラシ",
+      "check": false,
+      "heart": "♥"
+    },]    
+  }else if (index===2) {
+    return[
+      {
+        "item": "玄関の戸締りした？",
+        "check": false,
+        "heart": "♥♥♥"
+      },
+      {
+      "item": "窓の鍵閉めた？",
+      "check": false,
+      "heart": "♥♥♥"
+    },
+    {
+      "item": "ガス元栓閉めた？",
+      "check": false,
+      "heart": "♥♥♥"
+    },
+    {
+      "item": "スマホ持った？",
+      "check": false,
+      "heart": "♥♥♥"
+    },
+    {
+      "item": "財布持った？",
+      "check": false,
+      "heart": "♥♥♥"
+    },
+    {
+      "item": "目覚し時計のスヌーズ消した？",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "カーテン閉めた？",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "テレビ消した？",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "電気消した？",
+      "check": false,
+      "heart": "♥♥"
+    },
+    {
+      "item": "エアコン消した？",
+      "check": false,
+      "heart": "♥♥"
+    },]  
+  }else{
+    return[{
+      "item": "新しい項目を追加する",
+      "check": false,
+      "heart": "♥♥♥"
+    },]
+  }
+}
+
+
+const app = new Vue({
+  el:'#app',
+  data:{
+    isActive:0,
+    tabs:['','','',],
+    tabNames:['','','',],
+    header:false,
+    articles:['','','',],
+    inputItems:['','','',],
+    items:[[],[],[]],
+    hearts:'♥',
+    sort:['','','',],
+  },
+
+  watch:{
+    sort:{
+      handler:function(val){
+      const sortName =function(a,b){
+        return a.item.localeCompare(b.item);
+      }
+      const sortImp =function(a,b){
+        if (a.heart > b.heart) {
+          return -1;
+        } else {
+          return 1;
+        }
+      }
+      const sortImpSub =function(a,b){
+        if (a.heart > b.heart) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+      const sortCheck =function(a,b){
+        if (a.check > b.check) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+      if (val[this.isActive]==='名前順') {
+          this.items[this.isActive].sort(sortName)
+      }else if (val[this.isActive]==='重要度順') {
+          this.items[this.isActive].sort(sortImp)
+      }else if (val[this.isActive]==='未対応順') {
+        this.items[this.isActive].sort(sortImpSub)
+        this.items[this.isActive].sort(sortCheck)
+      }
+    }, 
+    deep: true 
+  },
+
+  tabs:{
+    handler:function(){
+    localStorage.setItem('LocalTabs', JSON.stringify(this.tabs));
+  },
+  deep: true 
+  },
+  
+  tabNames:{
+    handler:function(){
+    localStorage.setItem('LocalTabNames', JSON.stringify(this.tabNames));
+  },
+  deep: true 
+  },
+
+  articles:{
+    handler:function(){
+    localStorage.setItem('LocalArticles', JSON.stringify(this.articles));
+  },
+  deep: true 
+  },
+
+  items: {  
+    handler: function() { 
+        localStorage.setItem('LocalItems', JSON.stringify(this.items));  
+  },  
+    deep: true  
+  },
+
+  },
+
+  mounted: function() { 
+    this.tabs = JSON.parse(localStorage.getItem('LocalTabs')) || ['','','',]
+
+    this.articles = JSON.parse(localStorage.getItem('LocalArticles')) || ['','','',]
+
+    this.items = JSON.parse(localStorage.getItem('LocalItems')) || [defaultItems(0),defaultItems(1),defaultItems(2)];
+
+    this.tabNames = JSON.parse(localStorage.getItem('LocalTabNames')) || ['Camp','Travel','Before leaving home',];
+    },
+
+  methods:{
+    change:function(index){
+      this.isActive=index;
+      if (this.isActive > this.tabs.length-1) {
+        this.isActive=this.tabs.length-1
+      }
+    },
+    
+    addTab:function(){
+      this.tabs.push('')
+      this.tabNames.push('New')
+      this.articles.push('')
+      this.items.push(defaultItems())
+      this.isActive=this.tabs.length-1
+    },
+
+    deleteTab:function(index){
+      let result = confirm('このタブを削除しますか？');
+      if (result) {
+      this.tabs.splice(index,1)
+      this.tabNames.splice(index,1)
+      this.articles.splice(index,1)
+      this.items.splice(index,1)
+      this.sort.splice(index,1)
+      this.isActive=index-1
+      }
+    },
+
+    headderActive:function(){
+      if (this.header===false) {
+        this.header=true
+      }else{
+        this.header=false
+      }
+    },
+
+    addItem:function(index){
+      if(this.inputItems[index]===''){
+        return
+      }else if(this.isActive===index){
+        let newItem={
+          item:this.inputItems[index],
+          check:false,
+          heart:this.hearts,
+        }
+        this.items[index].push(newItem);
+        this.inputItems[index]='';
+        this.hearts='♥';
+      }
+    },
+
+    checkItem:function(index,num){
+      let item=this.items[index]
+        if (this.isActive===index) {
+          if (item[num].check===true) {
+            item[num].check=false;
+          }else{
+            item[num].check=true;
+          }
+        }
+    },
+
+    deleteItem:function(index,num){
+      let item=this.items[index]
+      if (this.isActive===index) {
+          item.splice(num,1);
+      }
+    },
+
+    dragList(event,dragIndex){
+      event.dataTransfer.effectAllowed = 'move'
+      event.dataTransfer.dropEffect = 'move'
+      event.dataTransfer.setData('drag-index',dragIndex)
+    },
+    dropList(event,itemIndex,dropIndex){
+      let item=this.items[itemIndex]
+      let dragIndex = event.dataTransfer.getData('drag-index')
+      if (this.isActive===itemIndex) {
+        const deleteList = item.splice(dragIndex, 1);
+        item.splice(dropIndex, 0, deleteList[0])
+      }
+    },
+    
+    allItemsClear:function(index){
+      if (this.isActive===index) {
+        this.items[index].forEach(newItem => {
+          newItem.check=false
+        });
+      }
+    },
+
+    allItemsDelete:function(index){
+      let result = confirm('このタブの全てのアイテムを削除しますか？');
+      if (result) {
+        if (this.isActive===index) {
+          this.items[index].splice(0);
+        }
+      }
+    },
+
+    defaultItems:function(index){
+      let result = confirm('このタブの全てのアイテムを初期設定に戻しますか？');
+      if (result) {
+        if (this.isActive===index) {
+          this.items.splice(index,1,(defaultItems(index)));
+        }
+      }
+    },
+
+
+  }
+})
